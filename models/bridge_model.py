cc440de0932e20c559a561fbf534af18d57224ce
Nexusodec <Brycetan77@gmail.com>
2026-03-30 17:59:16 +0800

    Removed extra commented part in bridge_model.py

--- models/bridge_model.py
+++ models/bridge_model.py
@@ -1,29 +1,3 @@
-
-from config import *
-
-# def fit_bridge_model(data, selected_names, target_col='GDP_growth', verbose = VERBOSE):
-#     """
-#     Fit OLS bridge equation: GDP_growth ~ selected indicators (quarterly aggregates).
-#     Returns the fitted model and a dictionary of coefficients.
-#     """
-#     mdata = data[selected_names + [target_col]].copy()
-
-#     mdata = mdata.replace([np.inf, -np.inf], np.nan).dropna()
-
-#     X_sel = mdata[selected_names]
-#     X_sel = sm.add_constant(X_sel)
-#     y = mdata['GDP_growth']
-#     model = sm.OLS(y, X_sel).fit()
-#     if verbose:
-#         print(model.summary())
-    
-#     # Store coefficients
-#     coef_dict = {'intercept': model.params['const']}
-#     for name in selected_names:
-#         coef_dict[name] = model.params[name]
-#     return model, coef_dict
-
-
 from config import *
 import pandas as pd
 import numpy as np
